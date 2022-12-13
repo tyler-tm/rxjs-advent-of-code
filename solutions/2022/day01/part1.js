ec1bc8455678ec8131fc30242862c6a0c92c0b26
Tyler Maklebust <tmaklebust@gmail.com>
2022-12-13 17:32:34 -0500

    Cleans up Day 1

--- solutions/2022/day01/part1.js
+++ solutions/2022/day01/part1.js
@@ -1,15 +1,16 @@
 import { readFile } from "./utils/inputParser.js"
-import { mergeMap, reduce } from "rxjs"
+import { mergeMap, reduce, takeLast } from "rxjs"
 
 const YEAR = 2022
 const DAY = 1
 const TRIM_LAST = false
 
-export const solve = readFile(YEAR, DAY, TRIM_LAST)
-    .pipe(reduce(
+export const solve = readFile(YEAR, DAY, TRIM_LAST).pipe(
+    reduce(
         (a, c) => c === ''
             ? [a[0].concat([a[1]]), 0]
             : [a[0], a[1] + parseInt(c)],
-        [[], 0]))
-    .pipe(mergeMap(x => x[0].sort((a, b) => a - b)))
-    .pipe(takeLast(1))
+        [[], 0]),
+    mergeMap(x => x[0].sort((a, b) => a - b)),
+    takeLast(1)
+)
--- solutions/2022/day01/part2.js
+++ solutions/2022/day01/part2.js
@@ -1,16 +1,17 @@
 import { readFile } from "./utils/inputParser.js"
-import { map, mergeMap, reduce, takeLast } from "rxjs"
+import { mergeMap, reduce, takeLast } from "rxjs"
 
 const YEAR = 2022
 const DAY = 1
 const TRIM_LAST = false
 
-export const solve = readFile(YEAR, DAY, TRIM_LAST)
-    .pipe(reduce(
+export const solve = readFile(YEAR, DAY, TRIM_LAST).pipe(
+    reduce(
         (a, c) => c === ''
             ? [a[0].concat([a[1]]), 0]
             : [a[0], a[1] + parseInt(c)],
-        [[], 0]))
-    .pipe(mergeMap(x => x[0].sort((a, b) => a - b)))
-    .pipe(takeLast(3))
-    .pipe(reduce((a, c) => a + c))
+        [[], 0]),
+    mergeMap(x => x[0].sort((a, b) => a - b)),
+    takeLast(3),
+    reduce((a, c) => a + c)
+)
